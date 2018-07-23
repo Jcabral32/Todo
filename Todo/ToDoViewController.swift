daa665be953ad43918b647a4508bcb0d2bd76403
jcabral32 <jcabral18@gmail.com>
2018-07-23 18:20:54 -0400

    Set up TableView Controller and Navigation Controller.

---
+++ Todo/ToDoViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  Todo
+//
+//  Created by Jean Cabral on 7/23/18.
+//  Copyright © 2018 Jean Cabral. All rights reserved.
+//
+
+import UIKit
+
+class ToDoViewController: UITableViewController {
+
+    
+    let itemArray = ["item1", "item2", "item3"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: TableView Data Source Methods
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    
+    // Returns a Cell
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+    
+    // MARK: TableView Delegate Methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print(itemArray[indexPath.row])
+      
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            
+        tableView.cellForRow(at: indexPath)?.accessoryType = .none
+            
+        } else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+}
+
